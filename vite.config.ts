e7c5ef326b4d7c620d9932c71122d1b5f992df36
SolamMedia <solammedia@example.com>
2025-11-09 10:26:49 +0100

    fix: eliminar headers CORS (single-threaded no los necesita)

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,6 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 5173,
         host: '0.0.0.0',
-        headers: {
-          'Cross-Origin-Embedder-Policy': 'credentialless',
-          'Cross-Origin-Opener-Policy': 'same-origin',
-        },
       },
       plugins: [react()],
       define: {
